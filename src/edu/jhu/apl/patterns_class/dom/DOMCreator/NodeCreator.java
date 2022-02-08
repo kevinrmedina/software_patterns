4a28e1ab79164e5c54c28ece7f9eb5afce89b3b4
kevinrmedina <kevinrmedina@protonmail.com>
2022-02-07 23:09:38 -0500

    Removed old Creator classes. Unnecessary.

--- src/edu/jhu/apl/patterns_class/dom/DOMCreator/NodeCreator.java
+++
@@ -1,9 +0,0 @@
-package edu.jhu.apl.patterns_class.dom.DOMCreator;
-
-import edu.jhu.apl.patterns_class.dom.Node;
-
-public class NodeCreator{
-    public Node createDOM(String name, short type){
-        return new Node(name, type);
-    }
-}
